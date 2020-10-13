dcb8902176f1b2dd7af33e8c67cf9d57120d1ed5
Farid Azouaou <28296521+Aqvayli06@users.noreply.github.com>
2020-10-13 10:01:04 +0200

    css , contact form for netlifly

--- single_blog_generator.R
+++ single_blog_generator.R
@@ -3,4 +3,4 @@ Saldae_single_post_generator <- function(Post_title = "I don't know"){
   distill::create_post(Post_title)  
 }
 
-Saldae_single_post_generator(Post_title = "End-to-End R-based Prod projects")
+Saldae_single_post_generator(Post_title = "End-to-End R-based prod Analytics projects(Part 2)")
